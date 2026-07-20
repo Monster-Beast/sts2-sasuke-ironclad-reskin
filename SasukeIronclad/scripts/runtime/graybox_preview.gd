7b1e70f1e89c1fe39536548888a07a8796b70259
Monster-Beast <111898334+Monster-Beast@users.noreply.github.com>
2026-07-20 18:03:27 +0800

    Preview Whirlwind and Fiend Fire loops

--- SasukeIronclad/scripts/runtime/graybox_preview.gd
+++ SasukeIronclad/scripts/runtime/graybox_preview.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var director: SasukeAnimationDirector = $SasukeAnimationRuntime/AnimationDirector
 @onready var status_label: Label = $Interface/Panel/Margin/VBox/Status
 @onready var low_flash_toggle: CheckButton = $Interface/Panel/Margin/VBox/LowFlash
+@onready var fast_toggle: CheckButton = $Interface/Panel/Margin/VBox/FastMode
 @onready var empowered_toggle: CheckButton = $Interface/Panel/Margin/VBox/Empowered
 
 func _ready() -> void:
@@ -12,6 +13,8 @@ func _ready() -> void:
     $Interface/Panel/Margin/VBox/Bash.pressed.connect(_play.bind("bash_sharingan_breaker"))
     $Interface/Panel/Margin/VBox/Cleave.pressed.connect(_play.bind("cleave_chidori_ground_arc"))
     $Interface/Panel/Margin/VBox/HeavyBlade.pressed.connect(_play.bind("heavy_blade_lightning_execution"))
+    $Interface/Panel/Margin/VBox/Whirlwind.pressed.connect(_play.bind("whirlwind_chidori_blade_storm"))
+    $Interface/Panel/Margin/VBox/FiendFire.pressed.connect(_play.bind("fiend_fire_dragon_flame_annihilation"))
     $Interface/Panel/Margin/VBox/Cancel.pressed.connect(_cancel)
     director.timeline_started.connect(_on_started)
     director.impact.connect(_on_impact)
@@ -23,14 +26,30 @@ func _play(animation_id: String) -> void:
     var variant := "base"
     if low_flash_toggle.button_pressed:
         variant = "low_flash"
+    elif fast_toggle.button_pressed:
+        variant = "fast"
     elif empowered_toggle.button_pressed:
         variant = "empowered"
-    status_label.text = "Loading %s (%s)…" % [animation_id, variant]
+
+    var hit_count := 1
+    var energy_spent := 1
+    var exhausted_card_count := 1
+    if animation_id == "whirlwind_chidori_blade_storm":
+        energy_spent = 5 if empowered_toggle.button_pressed else 3
+        hit_count = energy_spent
+    elif animation_id == "fiend_fire_dragon_flame_annihilation":
+        exhausted_card_count = 7 if empowered_toggle.button_pressed else 4
+        hit_count = exhausted_card_count
+
+    status_label.text = "Loading %s (%s, %d impacts)…" % [animation_id, variant, hit_count]
     director.play_timeline(animation_id, variant, {
         "low_flash": low_flash_toggle.button_pressed,
         "quality_scale": 1.0,
         "strength": 10 if empowered_toggle.button_pressed else 0,
-        "target_count": 3
+        "target_count": 3,
+        "energy_spent": energy_spent,
+        "exhausted_card_count": exhausted_card_count,
+        "hit_count": hit_count
     })
 
 func _cancel() -> void:
